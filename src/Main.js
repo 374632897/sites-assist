9aec1730722a5c41da9325421108d0651d7c364f
374632897 <374632897@qq.com>
2017-11-04 13:56:37 +0800

    update

--- src/Main.js
+++ src/Main.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react'
-
+import './main.css'
 const inputState = {
   path: '/m/list/',
-  st: 'http://bddeal.meishi.st.sankuai.com',
-  prod: 'http://bddeal.sankuai.com',
-  dev: 'http://bddeal.meishi.dev.sankuai.com',
-  local: 'http://localhost:8080',
-  test: 'http://bddeal.meishi.st.sankuai.com',
+  st: 'http://service.st.example.com',
+  prod: 'http://service.example.com',
+  dev: 'http://service.dev.example.com',
+  local: 'http://localhost:3000',
+  test: 'http://service.test.example.com',
   title: '我的项目',
 };
-const headers = Object.keys(inputState);
+const envDesc = 'st,prod,dev,local,test'.split(',');
+const pathDesc = ['path', 'title'];
+const headers = envDesc.concat(pathDesc);
 const initialState = {
   input: inputState,
   pathList: [], // { path: '', title: '', }
 };
-
+const tbodyHeaders = pathDesc.concat(envDesc).concat('ops')
 
 class Main extends Component {
   constructor (...args) {
@@ -39,7 +41,7 @@ class Main extends Component {
       <span>{field}</span>
       <input value={this.state.input[field]} onChange={(e) => {
         this.updateInput(field, e.target.value);
-      }} />
+      }} title={this.state.input[field]} />
     </label>)
   }
   renderTHeader (field) {
@@ -54,14 +56,30 @@ class Main extends Component {
         return item[field];
       default:
         if (!this.state.input[field]) return '';
-        return <a href={`${this.state.input[field]}${item.path}`}>{field}</a>
+        return (<a
+          target="_blank"
+          href={`${this.state.input[field]}${item.path}`}
+        >{field}</a>)
     }
+  }
+
+  removePath (item) {
+    this.setState({
+      ...this.state,
+      pathList: this.state.pathList.filter(path =>
+        path.path !== item.path &&
+        item.title !== path.title
+      ),
+    });
   }
   renderTBody (item, index) {
     return (<tr key={index}>
-      {headers.map(field => (<td key={field}>
+      {tbodyHeaders.slice(0, -1).map(field => (<td key={field}>
         {this.getText(item, field)}
       </td>))}
+      <td>
+        <span className="danger" onClick={this.removePath.bind(this, item)}>删除</span>
+      </td>
     </tr>)
   }
 
@@ -77,14 +95,17 @@ class Main extends Component {
     return (
       <div className="main">
         <header>
-          {headers.map(this.renderInput)}
-          <button onClick={this.handleAdd}>添加</button>
+          <div>{envDesc.map(this.renderInput)}</div>
+          <div>
+            {pathDesc.map(this.renderInput)}
+            <button onClick={this.handleAdd}>添加</button>
+          </div>
         </header>
         <section>
           <table>
             <thead>
               <tr>
-                {headers.map(this.renderTHeader)}
+                {tbodyHeaders.map(this.renderTHeader)}
               </tr>
             </thead>
             <tbody>
